34e86e34b2de54ec765f529e4807612f8ca08aff
EliseuMessias <eliseu.messias.brito@gmail.com>
2018-04-04 21:44:31 -0300

    Criando o Projeto

--- WordinOn/WordinOn.DataAccess/SalaXProfessorDAO.cs
+++ WordinOn/WordinOn.DataAccess/SalaXProfessorDAO.cs
@@ -1,13 +1,112 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WordinOn.Models;
 
 namespace WordinOn.DataAccess
 {
-    public class SalaXProfessor
+    public class SalaXProfessorDAO
     {
+        #region Inserir
+        public void Inserir(SalaXProfessor obj)
+        {
+            using (SqlConnection conn = new SqlConnection(@"Initial Catalog=WordinOnDB;
+                                                            Data Source=localhost;
+                                                            Integrated Security=SSPI;"))
+            {
+                string strSQL = @"insert into salaXprofessor (codProfessor, codSala) 
+                                    values (@codProfessor, @codSala);";
 
+                using (SqlCommand cmd = new SqlCommand(strSQL))
+                {
+                    cmd.Connection = conn;
+                    cmd.Parameters.Add("@codProfessor", SqlDbType.VarChar).Value = obj.Professor;
+                    cmd.Parameters.Add("@codSala", SqlDbType.VarChar).Value = obj.Sala;
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+        #endregion
+
+        #region Tirar da Sala
+        public void TirarDaSala(SalaXProfessor obj)
+        {
+            using (SqlConnection conn = new SqlConnection(@"Initial Catalog=WordinOnDB;
+                                                            Data Source=localhost;
+                                                            Integrated Security=SSPI;"))
+            {
+                string strSQL = @"delete from SalaXProfessor where codProfessor = @codProfessor and codSala = @codSala";
+
+                using (SqlCommand cmd = new SqlCommand(strSQL))
+                {
+                    cmd.Connection = conn;
+                    cmd.Parameters.Add("@codProfessor", SqlDbType.VarChar).Value = obj.Professor;
+                    cmd.Parameters.Add("@codSala", SqlDbType.VarChar).Value = obj.Sala;
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+        #endregion
+
+        #region Buscar Todos
+        public List<SalaXProfessor> BuscarTodos()
+        {
+            var lst = new List<SalaXProfessor>();
+
+            using (SqlConnection conn = new SqlConnection(@"Initial Catalog=WordinOnDB;
+                                                            Data Source=localhost;
+                                                            Integrated Security=SSPI;"))
+            {
+                string strSQL = @"select 
+                                    nome, 
+                                    email 
+                                    from SalaXProfessor 
+                                    inner join Usuario on SalaXProfessor.codProfessor = Usuario.cod";
+
+                using (SqlCommand cmd = new SqlCommand(strSQL))
+                {
+                    conn.Open();
+                    cmd.Connection = conn;
+                    cmd.CommandText = strSQL;
+
+                    var dataReader = cmd.ExecuteReader();
+                    var dt = new DataTable();
+                    dt.Load(dataReader);
+
+                    conn.Close();
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        var SalaXProfessor = new SalaXProfessor()
+                        {
+                            Professor = new Usuario()
+                            {
+                                Cod = Convert.ToInt32(row["cod"]),
+                                Nome = row["nome"].ToString(),
+                                Email = row["email"].ToString()
+                            },
+                            Sala = new Sala()
+                            {
+                                Cod = Convert.ToInt32(row["cod"]),
+                                Nome = row["nome"].ToString()
+                            }
+                        };
+                        lst.Add(SalaXProfessor);
+                    }
+                }
+            }
+            return lst;
+        }
+        #endregion
     }
 }
