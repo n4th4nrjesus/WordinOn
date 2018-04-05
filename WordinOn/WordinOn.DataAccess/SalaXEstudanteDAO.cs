052f9ce3808b0aa9692e65a6e3253600899b2614
n4th4nrjesus <n4th4nrjesus@gmail.com>
2018-04-04 21:29:13 -0300

    Criando o Projeto

--- WordinOn/WordinOn.DataAccess/SalaXEstudanteDAO.cs
+++ WordinOn/WordinOn.DataAccess/SalaXEstudanteDAO.cs
@@ -36,15 +36,14 @@ namespace WordinOn.DataAccess
         }
         #endregion
 
-        #region Excluir
-        public void Excluir(SalaXEstudante obj)
+        #region Tirar da Sala
+        public void TirarDaSala(SalaXEstudante obj)
         {
             using (SqlConnection conn = new SqlConnection(@"Initial Catalog=WordinOnDB;
                                                             Data Source=localhost;
                                                             Integrated Security=SSPI;"))
             {
-                string strSQL = @"insert into Usuario (codEstudante, codSala) 
-                                    values (@codEstudante, @codSala);";
+                string strSQL = @"delete from SalaXEstudante where codEstudante = @codEstudante and codSala = @codSala";
 
                 using (SqlCommand cmd = new SqlCommand(strSQL))
                 {
@@ -60,27 +59,55 @@ namespace WordinOn.DataAccess
         }
         #endregion
 
-        #region Sair da Sala
-        public void Excluir(SalaXEstudante obj)
+        #region Buscar Todos
+        public List<SalaXEstudante> BuscarTodos()
         {
+            var lst = new List<SalaXEstudante>();
+
             using (SqlConnection conn = new SqlConnection(@"Initial Catalog=WordinOnDB;
                                                             Data Source=localhost;
                                                             Integrated Security=SSPI;"))
             {
-                string strSQL = @"insert into Usuario (codEstudante, codSala) 
-                                    values (@codEstudante, @codSala);";
+                string strSQL = @"select 
+                                    nome, 
+                                    email 
+                                    from salaXestudante 
+                                    inner join Usuario on salaXestudante.codEstudante = Usuario.cod";
 
                 using (SqlCommand cmd = new SqlCommand(strSQL))
                 {
-                    cmd.Connection = conn;
-                    cmd.Parameters.Add("@codEstudante", SqlDbType.VarChar).Value = obj.Estudante;
-                    cmd.Parameters.Add("@codSala", SqlDbType.VarChar).Value = obj.Sala;
-
                     conn.Open();
-                    cmd.ExecuteNonQuery();
+                    cmd.Connection = conn;
+                    cmd.CommandText = strSQL;
+
+                    var dataReader = cmd.ExecuteReader();
+                    var dt = new DataTable();
+                    dt.Load(dataReader);
+
                     conn.Close();
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        var redacao = new Redacao()
+                        {
+                            Cod = Convert.ToInt32(row["cod"]),
+                            Estudante = new Usuario()
+                            {
+                                Cod = Convert.ToInt32(row["cod"]),
+                                Nome = row["Nome da Pessoa"].ToString()
+                            },
+                            Tema = new Tema()
+                            {
+                                Cod = Convert.ToInt32(row["cod"]),
+                                Nome = row["Tema Proposto"].ToString()
+                            },
+                            Data = Convert.ToDateTime(row["data"])
+                        };
+                        lst.Add(redacao);
+                    }
                 }
             }
+            return lst;
         }
         #endregion
 
