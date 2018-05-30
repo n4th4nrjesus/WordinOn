592028fb40abd8f6c23ddcaacaede8a1c3b129fd
n4th4nrjesus <n4th4nrjesus@gmail.com>
2018-05-29 21:40:12 -0300

    Corrigindo os erros

--- WordinOn/WordinOn.DataAccess/AvaliacaoDAO.cs
+++ WordinOn/WordinOn.DataAccess/AvaliacaoDAO.cs
@@ -7,131 +7,132 @@ using WordinOn.Models;
 
 namespace WordinOn.DataAccess
 {
-    public class AvaliacaoDAO
-    {
-        #region Inserir
-        public void Inserir(Avaliacao obj, int codProf)
-        {
-            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Db"].ConnectionString))
-            {
-                string strSQL = @"insert into Avaliacao (texto, valor, codProfessor, codRedacao)
+	public class AvaliacaoDAO
+	{
+		#region Inserir
+		public void Inserir(Avaliacao obj, int codProf)
+		{
+			using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Db"].ConnectionString))
+			{
+				string strSQL = @"insert into Avaliacao (texto, valor, codProfessor, codRedacao)
                                   values (@texto, @valor, @codProfessor, @codRedacao);";
 
-                using (SqlCommand cmd = new SqlCommand(strSQL))
-                {
-                    cmd.Connection = conn;
-                    cmd.Parameters.Add("@texto", SqlDbType.VarChar).Value = obj.Texto;
-                    cmd.Parameters.Add("@valor", SqlDbType.Int).Value = obj.Valor;
-                    cmd.Parameters.Add("@codProfessor", SqlDbType.Int).Value = codProf;
-                    cmd.Parameters.Add("@codRedacao", SqlDbType.Int).Value = obj.Redacao.Cod;
+				using (SqlCommand cmd = new SqlCommand(strSQL))
+				{
+					cmd.Connection = conn;
+					cmd.Parameters.Add("@texto", SqlDbType.VarChar).Value = obj.Texto;
+					cmd.Parameters.Add("@valor", SqlDbType.Int).Value = obj.Valor;
+					cmd.Parameters.Add("@codProfessor", SqlDbType.Int).Value = codProf;
+					cmd.Parameters.Add("@codRedacao", SqlDbType.Int).Value = obj.Redacao.Cod;
 
-                    conn.Open();
-                    cmd.ExecuteNonQuery();
-                    conn.Close();
-                }
-            }
-        }
-        #endregion
+					conn.Open();
+					cmd.ExecuteNonQuery();
+					conn.Close();
+				}
+			}
+		}
+		#endregion
 
-        #region Buscar Todos
-        public List<Avaliacao> BuscarTodos()
-        {
-            var lst = new List<Avaliacao>();
+		#region Buscar Todos
+		public List<Avaliacao> BuscarTodos()
+		{
+			var lst = new List<Avaliacao>();
 
-            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Db"].ConnectionString))
-            {
-                string strSQL = @"select 
+			using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Db"].ConnectionString))
+			{
+				string strSQL = @"select 
                                     a.*,
                                     u.nome as Nome_Professor
                                     from Avaliacao a
                                     inner join Usuario u on u.cod = a.codProfessor";
 
-                using (SqlCommand cmd = new SqlCommand(strSQL))
-                {
-                    conn.Open();
-                    cmd.Connection = conn;
-                    cmd.CommandText = strSQL;
+				using (SqlCommand cmd = new SqlCommand(strSQL))
+				{
+					conn.Open();
+					cmd.Connection = conn;
+					cmd.CommandText = strSQL;
 
-                    var dataReader = cmd.ExecuteReader();
-                    var dt = new DataTable();
-                    dt.Load(dataReader);
+					var dataReader = cmd.ExecuteReader();
+					var dt = new DataTable();
+					dt.Load(dataReader);
 
-                    conn.Close();
+					conn.Close();
 
-                    foreach (DataRow row in dt.Rows)
-                    {
-                        var avaliacao = new Avaliacao()
-                        {
-                            Cod = Convert.ToInt32(row["cod"]),
-                            Texto = row["texto"].ToString(),
-                            Valor = Convert.ToInt32(row["valor"]),
-                            Professor = new Usuario()
-                            {
-                                Cod = Convert.ToInt32(row["codProfessor"]),
-                                Nome = row["Nome_Professor"].ToString()
-                            },
-                            Redacao = new Redacao()
-                            {
-                                Cod = Convert.ToInt32(row["codRedacao"])
-                            }
-                        };
-                        lst.Add(avaliacao);
-                    }
-                }
-            }
-            return lst;
-        }
-        #endregion
+					foreach (DataRow row in dt.Rows)
+					{
+						var avaliacao = new Avaliacao()
+						{
+							Cod = Convert.ToInt32(row["cod"]),
+							Texto = row["texto"].ToString(),
+							Valor = Convert.ToInt32(row["valor"]),
+							Professor = new Usuario()
+							{
+								Cod = Convert.ToInt32(row["codProfessor"]),
+								Nome = row["Nome_Professor"].ToString()
+							},
+							Redacao = new Redacao()
+							{
+								Cod = Convert.ToInt32(row["codRedacao"])
+							}
+						};
+						lst.Add(avaliacao);
+					}
+				}
+			}
+			return lst;
+		}
+		#endregion
 
-        #region Buscar por Redação
-        public List<Avaliacao> BuscarPorRedacao(int cod)
-        {
-            var lst = new List<Avaliacao>();
+		#region Buscar por Redação
+		public List<Avaliacao> BuscarPorRedacao(int cod)
+		{
+			var lst = new List<Avaliacao>();
 
-            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Db"].ConnectionString))
-            {
-                string strSQL = @"select 
+			using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Db"].ConnectionString))
+			{
+				string strSQL = @"select 
                                     u.nome as Nome_Professor,
                                     a.*
                                     from Avaliacao a
                                     inner join Usuario u on u.cod = a.codProfessor
                                     where codRedacao = @codRedacao;";
 
-                using (SqlCommand cmd = new SqlCommand(strSQL))
-                {
-                    conn.Open();
-                    cmd.Connection = conn;
-                    cmd.Parameters.Add("@codRedacao", SqlDbType.Int).Value = cod;
-                    cmd.CommandText = strSQL;
+				using (SqlCommand cmd = new SqlCommand(strSQL))
+				{
+					conn.Open();
+					cmd.Connection = conn;
+					cmd.Parameters.Add("@codRedacao", SqlDbType.Int).Value = cod;
+					cmd.CommandText = strSQL;
 
-                    var dataReader = cmd.ExecuteReader();
-                    var dt = new DataTable();
-                    dt.Load(dataReader);
+					var dataReader = cmd.ExecuteReader();
+					var dt = new DataTable();
+					dt.Load(dataReader);
 
-                    conn.Close();
+					conn.Close();
 
-                    foreach (DataRow row in dt.Rows)
-                    {
-                        var avaliacao = new Avaliacao()
-                        {
-                            Cod = Convert.ToInt32(row["cod"]),
-                            Texto = row["texto"].ToString(),
-                            Valor = Convert.ToInt32(row["valor"]),
-                            Professor = new Usuario()
-                            {
-                                Cod = Convert.ToInt32(row["codProfessor"]),
-                                Nome = row["Nome_Professor"].ToString()
-                            },
-                            Redacao = new Redacao()
-                            {
-                                Cod = Convert.ToInt32(row["codRedacao"])
-                        };
-                        lst.Add(avaliacao);
-                    }
-                }
-            }
-            return lst;
-        }
-        #endregion
-    }
+					foreach (DataRow row in dt.Rows)
+					{
+						var avaliacao = new Avaliacao()
+						{
+							Cod = Convert.ToInt32(row["cod"]),
+							Texto = row["texto"].ToString(),
+							Valor = Convert.ToInt32(row["valor"]),
+							Professor = new Usuario()
+							{
+								Cod = Convert.ToInt32(row["codProfessor"]),
+								Nome = row["Nome_Professor"].ToString()
+							},
+							Redacao = new Redacao()
+							{
+								Cod = Convert.ToInt32(row["codRedacao"])
+							}
+						};
+						lst.Add(avaliacao);
+					}
+				}
+			}
+			return lst;
+		}
+		#endregion
+	}
 }
