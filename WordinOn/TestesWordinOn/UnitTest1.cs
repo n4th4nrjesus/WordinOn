9fb6316cc94a96b737f2d5403bb228d2ed094e53
n4th4nrjesus <n4th4nrjesus@gmail.com>
2018-06-08 22:15:48 -0300

    começo testes

---
+++ WordinOn/TestesWordinOn/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordinOn.Models;
+using WordinOn.DataAccess;
+
+namespace TestesWordinOn
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Redacao redacao = new Redacao();
+			RedacaoDAO redacaoDAO = new RedacaoDAO();
+
+			redacaoDAO.BuscarTodos();
+		}
+	}
+}
